f13ed28c0687058cc438d6cd8526007b494a6970
srivatsan2009 <71771607+srivatsan2009@users.noreply.github.com>
2021-01-27 18:53:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+song="";
+function preload(){
+song=loadSound("music.mp3");
+}
+
+function setup() {
+canvas=createCanvas(400,300);
+canvas.position(500,300);
+video=createCapture(VIDEO);
+video.hide();
+}
+
+function draw() {
+image(video,0,0,400,300);
+}
+
+function play() {
+song.play();
+song.setVolume(1);
+song.rate(1)
+}
+
+function stop() {
+song.stop();
+}
