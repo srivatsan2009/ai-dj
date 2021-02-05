c8b91d793794539d077408586beb71d68e8b4510
srivatsan2009 <71771607+srivatsan2009@users.noreply.github.com>
2021-02-05 19:50:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,15 +3,10 @@ rightwristy="";
 leftwristx="";
 leftwristy="";
 leftWrist_score="";
+rightWrist_score="";
 song="";
-song1="";
-song2="";
-song3="";
-song4="";
 function preload(){
-song=loadSound("alone.mp3");
-posenet=ml5.poseNet(video,modeloaded);
-posenet.on('pose',getposes);
+song=loadSound("music.mp3");
 }
 
 function setup() {
@@ -19,25 +14,25 @@ canvas=createCanvas(400,300);
 canvas.position(500,300);
 video=createCapture(VIDEO);
 video.hide();
+posenet=ml5.poseNet(video,modeloaded);
+posenet.on('pose',getposes);
 }
 
-function draw() {
-image(video,0,0,400,300);
-fill("red");
-stroke("yellowgreen");
-if(leftWrist_score>0.2){
-circle(leftwristx,leftwristy,50);
-leftwristyin_number=Number(leftwristy);
-round=floor(leftwristyin_number);volume=(round/600).toFixed(2);
-song.setVolume(volume);
-document.getElementById("vol_value").innerHTML="volume:"+volume;
+
+function play() {
+song.play();
+song.setVolume(1);
+song.rate(1)
 }
-}    
+
+function stop() {
+song.stop();
+}
 
 function modeloaded(){
 console.log("posenet model is loaded");
 }
-    
+
 function getposes(results){
 if(results.length>0){
 console.log(results);
@@ -49,5 +44,49 @@ leftwristy=results[0].pose.leftWrist.y;
 console.log(leftwristx,leftwristy);
 leftWrist_score=results[0].pose.keypoints[9].score;
 console.log(leftWrist_score);
+rightWrist_score=results[0].pose.keypoints[10].score;
+console.log(rightWrist_score);
 }
 }
+
+function draw() {
+image(video,0,0,400,300);
+fill("red");
+stroke("yellowgreen");
+if(leftWrist_score>0.2){
+circle(leftwristx,leftwristy,50);
+leftwristyin_number=Number(leftwristy);
+round=floor(leftwristyin_number);
+volume=(round/600).toFixed(2);
+song.setVolume(volume);
+document.getElementById("vol_value").innerHTML="volume:"+volume;
+}
+
+if(rightWrist_score>0.2) {
+circle(rightwristx,rightwristy,50);
+if(rightwristy>0&&rightwristy<=60) {
+song.rate(0.5);
+document.getElementById("speed_value").innerHTML="speed:0.5x";
+}
+
+else if(rightwristy>60&&rightwristy<=120) 
+{song.rate(1);
+document.getElementById("speed_value").innerHTML="speed:1x";
+}
+else if(rightwristy>120&&rightwristy<=180) 
+{song.rate(1.5);
+document.getElementById("speed_value").innerHTML="speed:1.5x";
+}
+
+else if(rightwristy>180&&rightwristy<=240) 
+{song.rate(2);
+document.getElementById("speed_value").innerHTML="speed:2x";
+}
+
+else if(rightwristy>240&&rightwristy<=300) 
+{song.rate(2.5);
+document.getElementById("speed_value").innerHTML="speed:2.5x";
+}
+}
+}
+  
